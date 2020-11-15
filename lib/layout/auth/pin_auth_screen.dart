99bf8df34e3807b8fdff98ae1e024d408db0d92a
Benedict Korie <benedictkorie@gmail.com>
2020-11-15 18:03:58 +0100

    pin_auth update

--- lib/layout/auth/pin_auth_screen.dart
+++ lib/layout/auth/pin_auth_screen.dart
@@ -30,8 +30,8 @@ class _PinAuthScreenState extends State<PinAuthScreen> {
     var otpField = PinPut(
       fieldsCount: 4,
       controller: otpController,
-      obscureText: "\u{02022}",
-      textStyle: TextStyle(fontSize: 30.0, height: 1.0),
+      // obscureText: "\u{02022}",
+      // textStyle: TextStyle(fontSize: 30.0, height: 1.0),
       autofocus: true,
       eachFieldConstraints: BoxConstraints(
         maxHeight: 30.0,
@@ -64,161 +64,149 @@ class _PinAuthScreenState extends State<PinAuthScreen> {
     );
 
     //
-    return Scaffold(
-      body: Stack(children: [
-        Container(
-          color: AppColors.accent,
-          width: Numbers.deviceWidth,
-          height: Numbers.deviceHeight,
-        ),
-        Positioned(
-          top: 0.0,
-          child: Image(
-            image: AssetImage('assets/imgs/dunija_bg.png'),
-            fit: BoxFit.fitHeight,
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: Scaffold(
+        body: Stack(children: [
+          Container(
+            color: AppColors.accent,
             width: Numbers.deviceWidth,
+            height: Numbers.deviceHeight,
           ),
-        ),
-        Positioned(
-          top: 0.0,
-          right: 0.0,
-          child: Image(
-            image: AssetImage('assets/imgs/top_right.png'),
-            width: 200.0,
-          ),
-        ),
-        Positioned(
-          top: 0.0,
-          right: -50.0,
-          child: Image(
-            image: AssetImage('assets/imgs/top_right.png'),
-            width: 200.0,
-          ),
-        ),
-        Positioned(
-          top: 40.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 10.0,
-              ),
-              Image(
-                image: AssetImage('assets/imgs/dunija.png'),
-                width: 120.0,
-              ),
-              SizedBox(
-                width: Numbers.deviceWidth - 230,
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          top: 150.0,
-          child: Padding(
-            child: Row(children: [
-              Container(child: Text(pageName, style: AppStyles.pageTitle)),
-              SizedBox(
-                width: 100.0,
-              ),
-            ]),
-            padding: EdgeInsets.only(left: 20.0),
-          ),
-        ),
-        Positioned(
-          bottom: 50.0,
-          child: Container(
-            width: Numbers.deviceWidth,
-            height: Numbers.deviceHeight * (3 / 4) - 80,
-            decoration: BoxDecoration(
-              color: AppColors.brightColorTrans2,
-              boxShadow: [
-                BoxShadow(
-                    color: Color(0x33000000),
-                    spreadRadius: 1.0,
-                    blurRadius: 15.0)
-              ],
+          Positioned(
+            top: 0.0,
+            child: Image(
+              image: AssetImage('assets/imgs/dunija_bg.png'),
+              fit: BoxFit.fitHeight,
+              width: Numbers.deviceWidth,
             ),
-            child: Column(
+          ),
+          Positioned(
+            top: 0.0,
+            right: 0.0,
+            child: Image(
+              image: AssetImage('assets/imgs/top_right.png'),
+              width: 200.0,
+            ),
+          ),
+          Positioned(
+            top: 0.0,
+            right: -50.0,
+            child: Image(
+              image: AssetImage('assets/imgs/top_right.png'),
+              width: 200.0,
+            ),
+          ),
+          Positioned(
+            top: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(
-                  height: 50.0,
+                  width: 10.0,
                 ),
-                Text(
-                  'Change Password',
-                  style: AppStyles.cursiveTitle,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Align(
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    width: Numbers.centerBoxWidth,
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Enter OTP sent to your email',
-                      style: AppStyles.catLabel,
-                    ),
-                  ),
+                Image(
+                  image: AssetImage('assets/imgs/dunija.png'),
+                  width: 120.0,
                 ),
                 SizedBox(
-                  height: 30.0,
+                  width: Numbers.deviceWidth - 230,
                 ),
-
-                //OTP Field
-                Container(width: Numbers.centerBoxWidth - 50, child: otpField),
-
                 SizedBox(
-                  height: 30.0,
+                  width: 20.0,
                 ),
-                Container(
-                    width: Numbers.centerBoxWidth / 2,
-                    height: Numbers.buttonHeight,
-                    child: FlatButton(
-                      onPressed: () {
-                        Timer(Duration(seconds: 2), () {});
-                      },
-                      child: Text(
-                        'Submit',
-                        style: AppStyles.whiteLabel,
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(Numbers.inputBorderRadius)),
-                      color: AppColors.darkAccent,
-                    )),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Remembered your Password? '),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return LoginScreen();
-                        }));
-                      },
-                      child: Text(
-                        'Login',
-                        style: AppStyles.boldBrownLabel,
-                      ),
-                    )
-                  ],
-                )
               ],
             ),
           ),
-        ),
-      ]),
+          Positioned(
+            top: 150.0,
+            child: Padding(
+              child: Row(children: [
+                Container(child: Text(pageName, style: AppStyles.pageTitle)),
+                SizedBox(
+                  width: 100.0,
+                ),
+              ]),
+              padding: EdgeInsets.only(left: 20.0),
+            ),
+          ),
+          Positioned(
+            bottom: 50.0,
+            child: Container(
+              width: Numbers.deviceWidth,
+              height: Numbers.deviceHeight * (3 / 4) - 80,
+              decoration: BoxDecoration(
+                color: AppColors.brightColorTrans2,
+                boxShadow: [
+                  BoxShadow(
+                      color: Color(0x33000000),
+                      spreadRadius: 1.0,
+                      blurRadius: 15.0)
+                ],
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Text(
+                    'Change Password',
+                    style: AppStyles.cursiveTitle,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Align(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      width: Numbers.centerBoxWidth,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Enter OTP sent to your email',
+                        style: AppStyles.catLabel,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+
+                  //OTP Field
+                  Container(
+                      width: Numbers.centerBoxWidth - 50, child: otpField),
+
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Container(
+                      width: Numbers.centerBoxWidth / 2,
+                      height: Numbers.buttonHeight,
+                      child: FlatButton(
+                        onPressed: () {
+                          Timer(Duration(seconds: 2), () {});
+                        },
+                        child: Text(
+                          'Submit',
+                          style: AppStyles.whiteLabel,
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                Numbers.inputBorderRadius)),
+                        color: AppColors.darkAccent,
+                      )),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
+
+  Future<bool> _onBackPressed() {}
 }
