6e326a764b75f861adf6f37df552bb238083dd36
Benedict Korie <benedictkorie@gmail.com>
2020-11-16 04:07:49 +0100

    kitchen_update

--- lib/layout/kitchen_screen.dart
+++ lib/layout/kitchen_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dunija/layout/auth/login_screen.dart';
 import 'package:dunija/settings/colors.dart';
 import 'package:dunija/settings/quantities.dart';
@@ -6,6 +8,7 @@ import 'package:dunija/widgets/interest_dropdown.dart';
 import 'package:dunija/widgets/textfield_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:dunija/settings/Appsettings.dart';
+import 'package:flutter/rendering.dart';
 
 class KitchenScreen extends StatefulWidget {
   @override
@@ -16,294 +19,272 @@ final style = TextStyle(color: AppSettings.bgColor);
 
 class _KitchenScreenState extends State<KitchenScreen> {
   //
-  var _passwordHidden = true;
 
-  //
-  var passwordController = TextEditingController();
-  var emailController = TextEditingController();
+  var pageName = 'Kitchen', foodTitle = 'Food Title';
 
-  var pageName = 'Kitchen';
+  Future<bool> _onBackPressed() {
+    //
+  }
+
+  var progressRate = 0.0;
+
+  var progressBar = LinearProgressIndicator();
+
+  computeProgress() {
+    var timer;
+    timer = Timer.periodic(Duration(milliseconds: 200), (t) {
+      //
+      progressRate += 1;
+      print(progressRate);
+      if (progressRate >= 100) {
+        timer.cancel();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    //computeProgress();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     //
-    void togglePasswordVisibility() {
-      setState(() {
-        _passwordHidden = !_passwordHidden;
-      });
-    }
-
-    //TextField TextStyle
-    final textStyle = TextStyle(
-      fontSize: 18.0,
-      color: AppSettings.primaryOrange,
-    );
-
-    //Button TextStyle
-    final btnTextStyle = TextStyle(
-      fontSize: 18.0,
-      color: AppSettings.primaryOrange,
-    );
+    var progress = Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          width: (Numbers.deviceWidth - 150) * 0.7, //(progressRate / 100),
+          height: 25.0,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  AppColors.accent,
+                  AppColors.whiteColor,
+                ]),
+            border: Border.all(color: AppColors.brightColor, width: 2.0),
+            borderRadius: BorderRadius.circular(
+              Numbers.smallBoxBorderRadius,
+            ),
+            image: DecorationImage(
+              image: AssetImage('assets/imgs/progress.png'),
+              fit: BoxFit.fitHeight,
+              repeat: ImageRepeat.repeatX,
+            ),
+          ),
+        ));
 
     //
-    return Scaffold(
-      body: Stack(children: [
-        Container(
-          color: AppColors.accent,
-          width: Numbers.deviceWidth,
-          height: Numbers.deviceHeight,
-        ),
-        Positioned(
-          top: 0.0,
-          child: Image(
-            image: AssetImage('assets/imgs/dunija_bg.png'),
-            fit: BoxFit.fitHeight,
-            width: Numbers.deviceWidth,
-          ),
-        ),
-        Positioned(
-          top: 0.0,
-          right: 0.0,
-          child: Image(
-            image: AssetImage('assets/imgs/top_right.png'),
-            width: 200.0,
-          ),
-        ),
-        Positioned(
-          top: 0.0,
-          right: -50.0,
-          child: Image(
-            image: AssetImage('assets/imgs/top_right.png'),
-            width: 200.0,
-          ),
-        ),
-        Positioned(
-          top: 40.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 10.0,
-              ),
-              Column(children: [
-                SizedBox(
-                  height: 10.0,
-                ),
-                InkWell(
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 80.0,
+          backgroundColor: AppColors.whiteColor,
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          title: Container(
+            child: Row(children: [
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.only(top: 12.0),
                   child: Icon(
                     Icons.arrow_back_ios,
-                    color: AppColors.whiteColor,
+                    color: AppColors.accent,
                   ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
                 ),
-              ]),
-              SizedBox(
-                width: 5.0,
+                onTap: () {
+                  //
+                },
               ),
               Image(
                 image: AssetImage('assets/imgs/dunija.png'),
-                width: 120.0,
-              ),
-              SizedBox(
-                width: Numbers.deviceWidth - 230,
+                width: 100.0,
               ),
               SizedBox(
                 width: 20.0,
               ),
-            ],
-          ),
-        ),
-        Positioned(
-          top: 150.0,
-          child: Padding(
-            child: Row(children: [
-              Container(child: Text(pageName, style: AppStyles.pageTitle)),
-              SizedBox(
-                width: 100.0,
-              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(Numbers.largeBoxBorderRadius),
+                    color: AppColors.accent),
+                width: Numbers.deviceWidth / 2 - 20,
+                height: 30.0,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Image(
+                      image: AssetImage('assets/imgs/icon.png'),
+                      width: 28.0,
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Text(
+                      foodTitle,
+                      style: AppStyles.whiteLabel,
+                    )
+                  ],
+                ),
+              )
             ]),
-            padding: EdgeInsets.only(left: 20.0),
           ),
         ),
-        Positioned(
-          bottom: 10.0,
-          child: Container(
-            width: Numbers.deviceWidth,
-            height: Numbers.deviceHeight * (3 / 4) - 30,
-            decoration: BoxDecoration(
-              color: AppColors.brightColorTrans2,
-              boxShadow: [
-                BoxShadow(
-                    color: Color(0x33000000),
-                    spreadRadius: 1.0,
-                    blurRadius: 15.0)
-              ],
-            ),
-            child: ListView(children: [
-              Column(
-                children: [
-                  SizedBox(
-                    height: 0.0,
-                  ),
-                  Text(
-                    'Join the Adventure',
-                    style: AppStyles.cursiveTitle,
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-
-                  //Full Name Textfield
-                  Align(
-                    child: Container(
-                      width: Numbers.centerBoxWidth,
-                      height: Numbers.buttonHeight,
-                      child: CustomeTextField(
-                        controller: emailController,
-                        hint: 'Full Name',
-                        icon: Icons.person,
-                        hidden: false,
+        body: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              child: Container(
+                width: Numbers.deviceWidth,
+                child: Column(
+                  children: [
+                    SizedBox(height: 5.0),
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      width: Numbers.deviceWidth <= 415.0
+                          ? Numbers.deviceWidth
+                          : 415.0,
+                      child: Text(
+                        'Step 1',
+                        style: AppStyles.titleStyle,
                       ),
                     ),
-                  ),
-
-                  SizedBox(
-                    height: 10.0,
-                  ),
-
-                  //Email Textfield
-                  Align(
-                    child: Container(
-                      width: Numbers.centerBoxWidth,
-                      height: Numbers.buttonHeight,
-                      child: CustomeTextField(
-                        controller: emailController,
-                        hint: 'Email',
-                        icon: Icons.email,
-                        hidden: false,
+                    Container(
+                      padding: EdgeInsets.all(15.0),
+                      alignment: Alignment.topCenter,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                            Numbers.mediumBoxBorderRadius),
+                        color: AppColors.accent,
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              AppColors.accent,
+                              AppColors.lightAccent,
+                            ]),
                       ),
-                    ),
-                  ),
-
-                  SizedBox(
-                    height: 10.0,
-                  ),
-
-                  //Password Textfield
-                  Align(
-                    child: Container(
-                      width: Numbers.centerBoxWidth,
-                      height: Numbers.buttonHeight,
-                      child: CustomeTextField(
-                        controller: passwordController,
-                        hint: 'Password',
-                        icon: Icons.lock,
-                        hidden: true,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-
-                  //Interest Textfield
-                  Align(
-                    child: Container(
-                      width: Numbers.centerBoxWidth,
-                      height: Numbers.buttonHeight,
-                      child: InterestsDropdown(
-                          // controller: passwordController,
-                          // hint: 'Password',
-                          // icon: Icons.favorite
-                          // hidden: true,
+                      width: Numbers.deviceWidth <= 415.0
+                          ? Numbers.deviceWidth
+                          : 415.0,
+                      height: 300.0,
+                      // ,
+                      margin: EdgeInsets.all(5.0),
+                      //Inner Image Container
+                      child: Column(
+                        children: [
+                          Container(
+                            width: Numbers.deviceWidth,
+                            height: 200.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                  Numbers.mediumBoxBorderRadius),
+                              color: AppColors.whiteColor,
+                              boxShadow: [
+                                BoxShadow(
+                                    color: AppColors.darkAccent,
+                                    spreadRadius: 1,
+                                    blurRadius: 25,
+                                    offset: Offset(0, 15))
+                              ],
+                              gradient: LinearGradient(
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  colors: [
+                                    AppColors.brightColor,
+                                    AppColors.whiteColor,
+                                  ]),
+                            ),
                           ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-
-                  Align(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      width: Numbers.centerBoxWidth,
-                      alignment: Alignment.center,
-                      child: Column(children: [
-                        Text(
-                          'By signing up, you agree to our',
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            InkWell(
-                              child: Text(
-                                'Terms of Service',
-                                style: AppStyles.boldBrownSmallLabel,
+                          SizedBox(
+                            height: 35.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              //Progress Bar
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                height: 15.0,
+                                width: Numbers.deviceWidth - 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(
+                                        Numbers.smallBoxBorderRadius),
+                                    gradient: LinearGradient(
+                                        begin: Alignment.bottomLeft,
+                                        end: Alignment.topRight,
+                                        colors: [
+                                          AppColors.whiteColor,
+                                          AppColors.brightColor
+                                        ])),
+                                child: progress,
+                                clipBehavior: Clip.hardEdge,
                               ),
-                            ),
-                            Text(' & '),
-                            InkWell(
-                              child: Text(
-                                'Privacy Policy',
-                                style: AppStyles.boldBrownSmallLabel,
+                              SizedBox(
+                                width: 10.0,
                               ),
-                            ),
-                          ],
-                        )
-                      ]),
+                              //Count-down Time
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                width: 50.0,
+                                color: AppColors.accent,
+                                padding: EdgeInsets.symmetric(vertical: 2.0),
+                                child: Text(
+                                  '2:30',
+                                  style: AppStyles.boldWhiteLabel,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-
-                  //Terms of Service and Privacy Policy
-                  Container(
-                      width: Numbers.centerBoxWidth / 2,
-                      height: Numbers.buttonHeight,
-                      child: FlatButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Register',
-                          style: AppStyles.whiteLabel,
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                Numbers.inputBorderRadius)),
-                        color: AppColors.darkAccent,
-                      )),
-
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Already in? '),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return LoginScreen();
-                          }));
-                        },
-                        child: Text(
-                          'Login',
-                          style: AppStyles.boldBrownLabel,
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      height: 200,
+                      color: AppColors.brightColor,
+                      child: ListView(
+                        // shrinkWrap: true,
+                        // scrollDirection: Axis.vertical,
+                        children: [
+                          Text('data1'),
+                          Text('data2'),
+                          Text('data3'),
+                          Text('data4'),
+                          Text('data5'),
+                          Text('data6'),
+                          Text('data7'),
+                          Text('data8'),
+                          Text('data9'),
+                          Text('data0')
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ]),
-          ),
+            ),
+            Positioned(
+                bottom: 0.0,
+                child: Container(
+                  width: Numbers.deviceWidth <= 415.0
+                      ? Numbers.deviceWidth
+                      : 415.0,
+                  height: 50.0,
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    color: AppColors.accent.withAlpha(50),
+                  ),
+                ))
+          ],
         ),
-      ]),
+      ),
     );
   }
 }
