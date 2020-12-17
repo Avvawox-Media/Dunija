0bf31493d893dd47682824771fd5a20c4c27b54e
Benedict Korie <benedictkorie@gmail.com>
2020-12-17 11:40:13 +0100

    kitchen exit dialog

--- lib/layout/dialog/infodialog.dart
+++ lib/layout/dialog/infodialog.dart
@@ -5,6 +5,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InfoDialog {
+  ///Show info, warning or error dialog
+  ///notifying the user of any of the afore-listed issues
   static showInfoDialog(
       {@required BuildContext context, @required title, @required msg}) {
     showDialog(
@@ -71,6 +73,7 @@ class InfoDialog {
     );
   }
 
+  ///Show Loading Dialog when performing heavy liftings
   static showLoadingDialog(BuildContext context) {
     showDialog(
       barrierDismissible: false,
@@ -98,6 +101,106 @@ class InfoDialog {
                 Text(
                   'Loading',
                   style: TextStyle(color: AppColors.whiteColor),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ///Show Dialog when Exit Button Clicked
+  static showExitKitchenDialog(
+      {@required BuildContext context, @required title, @required msg}) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => Material(
+        type: MaterialType.transparency,
+        child: Center(
+          // Aligns the container to center
+          child: Container(
+            // A simplified version of dialog.
+            width: 300.0,
+            // height: 100.0,
+            // color: Colors.green,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text(
+                    title,
+                    style: AppStyles.whiteTitleLabel,
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    msg,
+                    style: AppStyles.whiteBodyLabel,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+
+                ///Action Buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    //Exit Kitchen Action Button
+                    Align(
+                      child: Container(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                width: 1.0, color: AppColors.accentTrans),
+                            borderRadius: BorderRadius.circular(
+                              Numbers.largeBoxBorderRadius,
+                            ),
+                          ),
+                          color: AppColors.accent.withOpacity(0.8),
+                          child: Text(
+                            'Exit Anyway',
+                            style: AppStyles.whiteLabel,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                    // SizedBox(
+                    //   width: 15.0,
+                    // ),
+                    //Cancel Action Button
+                    Align(
+                      child: Container(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                width: 1.0, color: AppColors.darkAccentTrans),
+                            borderRadius: BorderRadius.circular(
+                              Numbers.largeBoxBorderRadius,
+                            ),
+                          ),
+                          color: AppColors.darkAccent.withOpacity(0.8),
+                          child: Text(
+                            'Cancel',
+                            style: AppStyles.whiteLabel,
+                          ),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
