9ee657d55f81f8bb2335b3211ce430bd7e5316ce
Benedict Korie <benedictkorie@gmail.com>
2020-12-24 08:38:13 +0100

    new - category listviews

---
+++ lib/layout/recipe_lists/baked_food_listview.dart
@@ -0,0 +1,46 @@
+import 'package:dunija/models/recipe.dart';
+import 'package:dunija/models/recipe_list_item.dart';
+import 'package:dunija/settings/custom_icon_icons.dart';
+import 'package:dunija/widgets/recipe_search_bar.dart';
+import 'package:flutter/material.dart';
+
+class BakedFoodListView extends StatelessWidget {
+  final Future<List<Recipe>> list;
+
+  BakedFoodListView(this.list);
+  //
+  final TextEditingController _recipeSearchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RecipeSearchBar(recipeSearchController: _recipeSearchController),
+        SizedBox(
+          height: 10.0,
+        ),
+        Expanded(
+          child: FutureBuilder(
+            future: list,
+            builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
+              if (!snapshot.hasData) {
+                return Container(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return ListView(
+                    padding: EdgeInsets.symmetric(vertical: 0.0),
+                    children: snapshot.data.map((e) {
+                      return RecipeListItem(
+                          title: e.name,
+                          category: e.description,
+                          image: Icon(CustomIcon.food));
+                    }).toList());
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/layout/recipe_lists/barbicue_listview.dart
@@ -0,0 +1,50 @@
+import 'package:dunija/layout/kitchen_screen.dart';
+import 'package:dunija/models/recipe.dart';
+import 'package:dunija/models/recipe_list_item.dart';
+import 'package:dunija/settings/colors.dart';
+import 'package:dunija/settings/custom_icon_icons.dart';
+import 'package:dunija/settings/quantities.dart';
+import 'package:dunija/settings/styles.dart';
+import 'package:dunija/widgets/recipe_search_bar.dart';
+import 'package:flutter/material.dart';
+
+class BarbicueListView extends StatelessWidget {
+  final Future<List<Recipe>> list;
+
+  BarbicueListView(this.list);
+  //
+  final TextEditingController _recipeSearchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RecipeSearchBar(recipeSearchController: _recipeSearchController),
+        SizedBox(
+          height: 10.0,
+        ),
+        Expanded(
+          child: FutureBuilder(
+            future: list,
+            builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
+              if (!snapshot.hasData) {
+                return Container(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return ListView(
+                    padding: EdgeInsets.symmetric(vertical: 0.0),
+                    children: snapshot.data.map((e) {
+                      return RecipeListItem(
+                          title: e.name,
+                          category: e.description,
+                          image: Icon(CustomIcon.food));
+                    }).toList());
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
