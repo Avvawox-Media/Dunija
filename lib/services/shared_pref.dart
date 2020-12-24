9526a0d8b0cacf1a7396a0a9547aececdbba49d1
Benedict Korie <benedictkorie@gmail.com>
2020-12-24 08:39:47 +0100

    new - settings pref storer

---
+++ lib/services/shared_pref.dart
@@ -0,0 +1,13 @@
+class SharedPref {
+  Future<bool> savePref(key, value) {
+    //Save
+  }
+
+  Future<dynamic> ReadPref(key) {
+    //Read
+  }
+
+  Future<bool> deletePref(key) {
+    //Delete
+  }
+}
