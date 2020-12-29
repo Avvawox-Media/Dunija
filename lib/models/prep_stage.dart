70512670374594eeb4fbc60ec471298b62221da5
Benedict Korie <benedictkorie@gmail.com>
2020-12-29 17:30:37 +0100

    update: preparation stage model

---
+++ lib/models/prep_stage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PrepStage {
+  final title, procedure, duration, image;
+  final int stageNo;
+
+  PrepStage(
+      {@required this.title,
+      @required this.procedure,
+      @required this.duration,
+      @required this.image,
+      @required this.stageNo});
+}
