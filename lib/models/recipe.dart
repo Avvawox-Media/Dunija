0f841af8907ccd1ae9c3b9bfa83fd4788232b76b
Benedict Korie <benedictkorie@gmail.com>
2020-12-29 17:31:23 +0100

    update: recipe model update

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,7 +1,14 @@
+import 'package:dunija/models/prep_stage.dart';
 import 'package:flutter/material.dart';
 
 class Recipe {
   final String name, description;
+  final int steps;
+  final List<PrepStage> stages;
 
-  Recipe({@required this.name, @required this.description});
+  Recipe(
+      {@required this.name,
+      @required this.description,
+      this.steps,
+      this.stages});
 }
