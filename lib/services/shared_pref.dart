811857293961d0dc1688db348731016e0c9dd29b
Benedict Korie <benedictkorie@gmail.com>
2021-01-04 15:12:35 +0100

    new: shareprefs implementation

--- lib/services/shared_pref.dart
+++ lib/services/shared_pref.dart
@@ -1,6 +1,21 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPref {
-  Future<bool> savePref(key, value) {
-    //Save
+  Future<bool> savePref(key, value) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    //Verify Data Type
+    if (value is int) {
+      prefs.setInt(key, value);
+    } else if (value is String) {
+      prefs.setString(key, value);
+    } else if (value is bool) {
+      prefs.setBool(key, value);
+    } else {
+      print("Invalid Type");
+    }
   }
 
   Future<dynamic> readPref(key) {
@@ -9,5 +24,20 @@ class SharedPref {
 
   Future<bool> deletePref(key) {
     //Delete
+  }
+
+  read(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return json.decode(prefs.getString(key));
+  }
+
+  save(String key, value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, json.encode(value));
+  }
+
+  remove(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove(key);
   }
 }
