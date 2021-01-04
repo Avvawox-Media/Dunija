db81a2242d1e436be0baa916b4dd149dda97e871
Benedict Korie <benedictkorie@gmail.com>
2021-01-04 15:10:52 +0100

    new: localized pref manager

---
+++ lib/services/ds8ns.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DS8ns implements WidgetsLocalizations {
+  @override
+  TextDirection get textDirection => TextDirection.ltr;
+
+  DS8ns.fromJson();
+
+  DS8ns.toJson();
+}
